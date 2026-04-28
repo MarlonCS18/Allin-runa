eda9eafa48ee308c287c4ea4f8d7c3bdc289d0c0
JohannChamorro20 <chamorro.mayta.johann.miguel@gmail.com>
2026-04-28 18:44:25 -0500

    feat: cambio titulo desde main

--- allin-runa/app/blog/page.js
+++ allin-runa/app/blog/page.js
@@ -57,7 +57,7 @@ export default function BlogPage() {
             initial="hidden"
             animate="visible"
           >
-            Nuestro Blog Orgánicos
+            Blog Natural Saludable
           </motion.h1>
           <motion.p 
             className="text-xl md:text-2xl"
